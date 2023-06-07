ff3fc3c879ef691d22e41ae315a11f01675b59e0
hjuanma <jm1995217@hotmail.com>
2023-06-07 04:13:12 -0500

    implemeted movies mapper

--- lib/infrastructure/datasources/moviesdb_datasource.dart
+++ lib/infrastructure/datasources/moviesdb_datasource.dart
@@ -1,8 +1,9 @@
 import 'package:dio/dio.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_respose.dart';
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/config/constants/themoviesdb.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
-
 import '../../domain/datasources/movies_datasource.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
@@ -15,7 +16,13 @@ class MoviedbDatasource extends MoviesDatasource {
   Future<List<Movie>> getNowPlaging({int page = 1}) async {
     final response =
         await dio.get("${TheMoviesDB.movie}${TheMoviesDB.nowPlaying}");
-    final List<Movie> movies = [];
+
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieDBResponse.results
+    .where((movieDb) => movieDb.posterPath != "no-poster")
+        .map((movieDb) => MovieMapper.movieDBToEntity(movieDb))
+        .toList();
     return movies;
   }
 }
