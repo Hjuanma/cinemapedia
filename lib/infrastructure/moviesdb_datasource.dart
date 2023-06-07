69017c06ed5b9af38445eab815ffc8814cb84cf4
hjuanma <jm1995217@hotmail.com>
2023-06-07 03:13:56 -0500

    dio and themovidb datasource added

---
+++ lib/infrastructure/moviesdb_datasource.dart
@@ -0,0 +1,21 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/config/constants/themoviesdb.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:dio/dio.dart';
+
+import '../domain/datasources/movies_datasource.dart';
+
+class MoviedbDatasource extends MoviesDatasource {
+  final dio = Dio(BaseOptions(baseUrl: TheMoviesDB.baseUrl, queryParameters: {
+    "api_key": Environment.theMovieDbKey,
+    "language": "es-MX"
+  }));
+
+  @override
+  Future<List<Movie>> getNowPlaging({int page = 1}) async {
+    final response =
+        await dio.get("${TheMoviesDB.movie}${TheMoviesDB.nowPlaying}");
+    final List<Movie> movies = [];
+    return movies;
+  }
+}
