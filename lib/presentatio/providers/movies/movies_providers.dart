7c82b0d63a1c8928dc0f1657d10072435f97f3f2
hjuanma <jm1995217@hotmail.com>
2023-06-13 04:12:32 -0500

    loading validation

--- lib/presentatio/providers/movies/movies_providers.dart
+++ lib/presentatio/providers/movies/movies_providers.dart
@@ -1,5 +1,5 @@
-import 'package:cinemapedia/presentatio/providers/movies/movies_repository_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/presentatio/providers/movies/movies_repository_provider.dart';
 
 import '../../../domain/entities/movie.dart';
 
@@ -13,14 +13,21 @@ typedef MovieCallback = Future<List<Movie>> Function({int page});
 
 class MoviesNotifier extends StateNotifier<List<Movie>> {
   int currentPage = 0;
+  bool isLoading = false;
   MovieCallback fetchMoreMovies;
 
   MoviesNotifier({required this.fetchMoreMovies}) : super([]);
 
   Future<void> loadNextPage() async {
+    if (isLoading) return;
+
+    isLoading = true;
     currentPage++;
 
     final List<Movie> movies = await fetchMoreMovies(page: currentPage);
     state = [...state, ...movies];
+    await Future.delayed(const Duration(milliseconds: 300));
+    
+    isLoading = false;
   }
 }
