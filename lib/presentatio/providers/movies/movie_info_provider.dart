4a56c9f2357c2ae2e9c3635f3caa7c94b3f9a8f7
hjuanma <jm1995217@hotmail.com>
2023-06-21 17:45:40 -0500

    Movie info provider added

---
+++ lib/presentatio/providers/movies/movie_info_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../domain/entities/movie.dart';
+import '../providerts.dart';
+
+final movieInfoProvider =
+    StateNotifierProvider<MovieMappNotifier, Map<String, Movie>>((ref) {
+  final getMovie = ref.watch(movieRepositoryProvider).getMovieById;
+  return MovieMappNotifier(getMovie: getMovie);
+});
+
+typedef GetMovieCallback = Future<Movie> Function(String movieId);
+
+class MovieMappNotifier extends StateNotifier<Map<String, Movie>> {
+  final GetMovieCallback getMovie;
+
+  MovieMappNotifier({required this.getMovie}) : super({});
+
+  Future<void> loadMovie(String movieId) async {
+    if (state[movieId] != null) return;
+
+    final movie = await getMovie(movieId);
+
+    state = {...state, movieId: movie};
+  }
+}
