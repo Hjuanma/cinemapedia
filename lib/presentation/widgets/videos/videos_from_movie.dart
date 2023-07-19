513c7f656b41bd130dec95ef36cf655afc8861c6
hjuanma <jm1995217@hotmail.com>
2023-07-19 17:16:42 -0500

    video added in a corousel

--- lib/presentation/widgets/videos/videos_from_movie.dart
+++ lib/presentation/widgets/videos/videos_from_movie.dart
@@ -1,46 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/domain/entities/entities.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-
-final FutureProviderFamily<List<Video>, int> videosFromMovieProvider = FutureProvider.family((ref, int movieId) {
+final FutureProviderFamily<List<Video>, int> videosFromMovieProvider =
+    FutureProvider.family((ref, int movieId) {
   final movieRepository = ref.watch(movieRepositoryProvider);
   return movieRepository.getYoutubeVideosById(movieId);
 });
 
 class VideosFromMovie extends ConsumerWidget {
-
   final int movieId;
 
   const VideosFromMovie({super.key, required this.movieId});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref ) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final moviesFromVideo = ref.watch(videosFromMovieProvider(movieId));
 
     return moviesFromVideo.when(
-      data: ( videos ) => _VideosList( videos: videos ),
-      error: (_ , __) => const Center(child: Text('No se pudo cargar películas similares') ), 
-      loading: () => const Center(child: CircularProgressIndicator(strokeWidth: 2)),
+      data: (videos) => _VideosList(videos: videos),
+      error: (_, __) =>
+          const Center(child: Text('No se pudo cargar películas similares')),
+      loading: () =>
+          const Center(child: CircularProgressIndicator(strokeWidth: 2)),
     );
   }
 }
 
 class _VideosList extends StatelessWidget {
-
   final List<Video> videos;
 
-  const _VideosList({required this.videos });
+  const _VideosList({required this.videos});
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
 
     //* Nada que mostrar
-    if ( videos.isEmpty ) {
-      return const SizedBox(); 
+    if (videos.isEmpty) {
+      return const SizedBox();
     }
 
     return Column(
@@ -49,31 +51,45 @@ class _VideosList extends StatelessWidget {
         // ignore: prefer_const_constructors
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: const Text('Videos', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+          child: const Text(
+            'Videos',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
         ),
 
-        ...videos.map(
-          (video) => _YouTubeVideoPlayer(youtubeId: video.youtubeKey, name: video.name)
-        ).toList()
+        SizedBox(
+          height: 250,
+          width: double.infinity,
+          child: Swiper(
+              viewportFraction: 1,
+              scale: 0.8,
+              autoplay: false,
+              pagination: SwiperPagination(
+                  margin: const EdgeInsets.only(top: 10),
+                  builder: DotSwiperPaginationBuilder(
+                      activeColor: colors.primary, color: colors.secondary)),
+              itemCount: videos.length,
+              itemBuilder: (context, index) => _YouTubeVideoPlayer(
+                  youtubeId: videos[index].youtubeKey,
+                  name: videos[index].name)),
+        )
       ],
     );
   }
 }
 
 class _YouTubeVideoPlayer extends StatefulWidget {
-
   final String youtubeId;
   final String name;
 
-  const _YouTubeVideoPlayer({ required this.youtubeId, required this.name });
+  const _YouTubeVideoPlayer({required this.youtubeId, required this.name});
 
   @override
   State<_YouTubeVideoPlayer> createState() => _YouTubeVideoPlayerState();
 }
 
 class _YouTubeVideoPlayerState extends State<_YouTubeVideoPlayer> {
-
-  late YoutubePlayerController _controller;  
+  late YoutubePlayerController _controller;
 
   @override
   void initState() {
@@ -95,8 +111,6 @@ class _YouTubeVideoPlayerState extends State<_YouTubeVideoPlayer> {
     );
   }
 
-
-
   @override
   void dispose() {
     _controller.dispose();
@@ -106,14 +120,10 @@ class _YouTubeVideoPlayerState extends State<_YouTubeVideoPlayer> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(widget.name),
-          YoutubePlayer(controller: _controller)
-        ],
-      )
-    );
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [Text(widget.name), YoutubePlayer(controller: _controller)],
+        ));
   }
 }
